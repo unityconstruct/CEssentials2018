aa0b720664dbc4d07ff59bdee4edeefbc4fec9c0
silosix <silosix@W7-DEV1>
2018-06-20 08:09:42 -0500

    About to implement PageObject Model, but Selenium3 has dropped support for annotations

---
+++ SeleniumFirst/EAPageObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumFirst
+{
+    class EAPageObject
+    {
+        public EAPageObject()
+        {
+
+        }
+
+
+        [FindsBy(How = How.Id, Using ="TitleId")]
+        public IWebElement TitleID { get; set; }
+
+
+        [FindsBy(How = How.Name, Using = "Initial")]
+        public IWebElement txtInitial { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Save")]
+        public IWebElement btnSave { get; set; }
+
+    }
+}
