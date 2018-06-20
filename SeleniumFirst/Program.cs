3212222127f3f4ba455691d39507ad4a90e8a330
silosix <silosix@W7-DEV1>
2018-06-20 04:21:15 -0500

    added new class to interact with the page controls

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static IWebDriver driver;
+        static IWebElement el;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+
+        public void Initialize()
+        {
+            //Create the reference for the browser
+            driver = new ChromeDriver(Directory.GetCurrentDirectory());
+        }
+
+        public void ExecuteTest()
+        {
+            //navigate to google page
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            //Find the element
+            el = driver.FindElement(By.Name("q"));
+
+            //Perform ops
+            el.SendKeys("unityconstruct");
+
+            Console.WriteLine(driver.Title);          //var title = driver.Title;
+        }
+
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Survey/Passcode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics
+{
+    public class Passcode
+    {
+        public static void AuthPasscode()
+        {
+            var code = "";
+            var counter = 1;
+            while ((code != "secret") && (counter < 4))
+            {
+                Console.WriteLine("What is the pass code?");
+                code = Console.ReadLine();
+                if (code != "secret")
+                {
+                    Console.WriteLine("Not Authenticated");
+                }
+                Console.WriteLine("Authenticated");
+                counter++;
+            }
+        }
+    }
+}
