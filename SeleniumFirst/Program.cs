bf9e8b084d51a9b77e2bc861bcb2d47ef7b27cf2
silosix <silosix@W7-DEV1>
2018-06-20 05:23:40 -0500

    added GetSeleniumMethods logic.. BUT dropdown menus get is returning the ID of the option, not the text

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -28,6 +28,7 @@ namespace SeleniumFirst
         public void ExecuteTest()
         {
 
+            // Enter(element,value,type) <-- put elements in new class
 
             //Title
             SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
@@ -35,19 +36,11 @@ namespace SeleniumFirst
             //Initial
             SeleniumSetMethods.EnterText(driver, "Initial", "executeA", "Name");
 
+            Console.WriteLine("The value from my Title is:" + SeleniumGetMethods.GetText(driver, "TitleId", "executeA", "Id"));
+            Console.WriteLine("The value from my Initial is:" + SeleniumGetMethods.GetText(driver, "Initial", "executeA", "Name"));
+
             //Click Save
             SeleniumSetMethods.Click(driver, "Save", "Name");
-            
-
-            // Enter(element,value,type) <-- put elements in new class
-
-            /*
-            //Find the element
-            el = driver.FindElement(By.Name("q"));
-
-            //Perform ops
-            el.SendKeys("unityconstruct");
-            */
 
             Console.WriteLine(driver.Title);          //var title = driver.Title;
             Console.WriteLine("Executed Test");
---
+++ SeleniumFirst/SeleniumGetMethods.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeleniumFirst
+{
+    class SeleniumGetMethods
+    {
+
+        public static string GetText(IWebDriver driver, string element, string value, string elementtype)
+        {
+
+            if (elementtype == "Id")
+                return driver.FindElement(By.Id(element)).GetAttribute("value");
+            if (elementtype == "Name")
+                return driver.FindElement(By.Name(element)).GetAttribute("value");
+            else return String.Empty;
+        }
+
+        public static string GetTextFromDDL(IWebDriver driver, string element, string value, string elementtype)
+        {
+            // RETURNS THE VALUE of the option NOT THE TEXT... need to investigate
+            if (elementtype == "Id")
+                return new SelectElement(driver.FindElement(By.Id(element))).AllSelectedOptions.SingleOrDefault().Text;
+            if (elementtype == "Name")
+                return new SelectElement(driver.FindElement(By.Name(element))).AllSelectedOptions.SingleOrDefault().Text;
+            else return String.Empty;
+        }
+    }
+}
