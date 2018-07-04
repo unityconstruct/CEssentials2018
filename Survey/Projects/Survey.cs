7467a09975ae2816cd810c02f76a362f0ef22e5d
silosix <silosix@W7-DEV1>
2018-07-04 02:49:03 -0500

    Updated Survey class to use Data class for storing/displaying data

--- Survey/Projects/Survey.cs
+++ Survey/Projects/Survey.cs
@@ -4,34 +4,49 @@ using System.Text;
 
 namespace Basics.Projects
 {
+
+    public class Data
+    {
+        public string Name;
+        public int Age;
+        public string Month;
+
+        public void Display()
+        {
+            Console.WriteLine("Your name is {0}", Name);
+            Console.WriteLine("Your age is {0}", Age);
+            Console.WriteLine("Your birth month is {0}", Month);
+
+            if (Month.ToLower() == "april")
+            {
+                Console.WriteLine("Sign is Aries");
+            }
+            else if (Month.ToLower() == "may")
+            {
+                Console.WriteLine("Sign is Taurus");
+            }
+        }
+        
+    }
     public class Survey
     {
         public static void AskSurvey()
         {
+            Data thisData = new Data();
+
             Console.WriteLine("What is your name?");
-            var name = TryAnswer();
+            thisData.Name = TryAnswer();
 
             Console.WriteLine("What is your age?");
-            var age = TryAnswer();
+            thisData.Age = int.Parse(TryAnswer());
 
             Console.WriteLine("What is your birth month?");
-            var month = TryAnswer();
+            thisData.Month = TryAnswer();
 
 
-            if (month.ToLower() == "april")
-            {
-                Console.WriteLine("Sign is Aries");
-            }
-            else if (month.ToLower() == "may")
-            {
-                Console.WriteLine("Sign is Taurus");
-            }
 
 
-            Console.WriteLine("Your name is {0}", name);
-            Console.WriteLine("Your age is {0}", age);
-            Console.WriteLine("Your birth month is {0}", month);
-
+            thisData.Display();
         }
 
         public static string TryAnswer()
