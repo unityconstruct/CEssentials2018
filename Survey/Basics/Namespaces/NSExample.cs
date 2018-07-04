4454c6eb53adce3b67c5cab6a6991ccb299e277a
silosix <silosix@W7-DEV1>
2018-07-04 02:11:35 -0500

    Stubbed out Basics.Namepages, ns1, ns2 namespaces & folders for future namespace examples

---
+++ Survey/Basics/Namespaces/NSExample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics.Basics.Namespaces
+{
+    class NSExample
+    {
+    }
+}
---
+++ Survey/Basics/Namespaces/ns2/Namespace2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics.Basics.Namespaces.ns2
+{
+    class Namespace2
+    {
+    }
+}
