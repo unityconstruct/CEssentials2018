713be650d39d1e3de42d566ba36bbe66669d83d6
silosix <silosix@W7-DEV1>
2018-07-04 03:00:57 -0500

    Added TryParse(eval,out) for Age & output error message if TryParse = false ( or fails )

--- Survey/Projects/EmployeeTrackerOO/EmployeeTracker.cs
+++ Survey/Projects/EmployeeTrackerOO/EmployeeTracker.cs
@@ -18,7 +18,14 @@ namespace Basics.Projects.EmployeeTrackerOO
                 var emp = new Employee();
 
                 emp.Name = Util.EmployeeTrackerUtils.Ask("Employee Name: ");
+                // Udpated for error handling on int.Parse
                 emp.Score = int.Parse(Util.EmployeeTrackerUtils.Ask("Employee Score: "));
+                bool result = int.TryParse(Util.EmployeeTrackerUtils.Ask("Employee Score: "), out emp.Score);
+                if (!result)
+                {
+                    Console.WriteLine("Age was not an error");
+                }
+
                 emp.Birthday = Util.EmployeeTrackerUtils.Ask("Employee Birthday: ");
                 emp.Address = Util.EmployeeTrackerUtils.Ask("Employee Address: ");
                 emp.Phone = double.Parse(Util.EmployeeTrackerUtils.Ask("Employee Phone: "));
@@ -38,44 +45,62 @@ namespace Basics.Projects.EmployeeTrackerOO
                 Console.WriteLine("Employee Name: {0} --- Employee Score: {1}", e.Name, e.Score);
             }
         }
+
+        public static void Import()
+        {
+            //uses Employee constructor for assignments
+            var importedEmployee = new Employee("ImportedEmployee", 12, "birthday", "Address", 1234567890);
+           // Console.WriteLine(importedEmployee.Name);
+        }
     }
 
-    class Employee
+
+    class Member
+    {
+        public string Name;
+        public string Address;
+        protected double phone;
+
+        public double Phone
+        {
+            set { phone = value; }
+        }
+    }
+
+    class Employee : Member
     {
         // ONE Field for ENTIRE CLASS, no matter how many instances exist
         // inits to 0 when CONSTUCTOR CALLED on first reference in the program
         public static int Count = 0; 
 
         // Fields are at class-level w/out get/sets
-        public string Name;
         public int Score;
         public string Birthday;
-        public string Address;
-        private double phone; //Properties have get/sets
 
         public Employee()
         {
-
+             
         }
 
         public Employee(string name, int score, string birthday, string address, double phone) //constructor
         {
-            Name = name;
             Score = score;
             Birthday = birthday;
-            Address = address;
-            Phone = phone;
-        }
-        public double Phone
-        {
-            set { phone = value; } 
         }
 
+    }
 
+    class Manager : Member
+    {
+        public static int Count = 0;
 
-        public void SetPhone(double number)
+        public string Name;
+        public string Address;
+        public string Dept;
+
+        public Manager()
         {
-            phone = number;
+
         }
     }
 }
